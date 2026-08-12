e88b27294d9747d6f78cc4578756e6ab940fdeb2
maviimedia <maviimedia@gmail.com>
2026-08-13 00:56:24 +0530

    klonoo base setup complete

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        background: 'src/background.js',
+        content: 'src/content.js'
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      }
+    }
+  }
 })
